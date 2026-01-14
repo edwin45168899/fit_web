0fe65ef17d088844b2d736d9e30e767e2e35ea85
chiisen <chiisen@hotmail.com>
2026-01-15 01:20:41 +0800

    feat: 新增 git push 腳本

---
+++ setup_git_sync.ps1
@@ -0,0 +1,26 @@
+# setup_git_sync.ps1
+# 依據 docs/git同步備份branch.md 建立的同步腳本
+
+$RepoName = "fit_web"
+
+$BackupUrl = "git@github.com:edwin45168899/$RepoName.git"
+$MainUrl = "git@github.com-chiisen:chiisen/$RepoName.git"
+$MainUrl1 = "git@github.com-edwiin1688:edwiin1688/$RepoName.git"
+$MainUrl2 = "git@github.com-NathanEvans1221:NathanEvans1221/$RepoName.git"
+
+Write-Output "正在設定 Git 遠端同步 (Repo: $RepoName)..."
+Write-Output "備份遠端: $BackupUrl"
+Write-Output "主要遠端: $MainUrl"
+Write-Output "主要遠端1: $MainUrl1"
+Write-Output "主要遠端2: $MainUrl2"
+
+# 1. 加入 backup repo 到 origin 的 push list
+git remote set-url --add --push origin $BackupUrl
+
+# 2. 加入主要 repo 到 origin 的 push list
+git remote set-url --add --push origin $MainUrl
+git remote set-url --add --push origin $MainUrl1
+git remote set-url --add --push origin $MainUrl2
+
+Write-Output "設定完成。目前的 remote 設定如下："
+git remote -v
